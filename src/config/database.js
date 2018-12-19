d99474d72c79dd9c2391ce61b5149c195146acbe
Daniel <daniel@ept.pt>
2018-12-19 19:42:38 +0000

    user validation

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import mongoose from 'mongoose';
 
 import constants from './constants';
--- src/modules/users/user.routes.js
+++ src/modules/users/user.routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import validate from 'express-validation';
+
 import * as userController from './user.controllers';
+import userValidation from './user.validations';
 
 const routes = new Router();
 
-routes.post('/signup', userController.signUp);
+routes.post('/signup', validate(userValidation.signup), userController.signUp);
 
 export default routes;
