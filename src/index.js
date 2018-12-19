460a3f2cb41014714c49721b4b471098295a5381
Daniel <daniel@ept.pt>
2018-12-19 17:30:21 +0000

    Setup ok

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import express from 'express';
 
+import constants from './config/constants';
+import './config/database';
+import middlewaresConfig from './config/middlewares';
+
 const app = express();
 
-app.listen(PORT, err => {
+middlewaresConfig(app);
+
+app.get('/', (req, res) => {
+  res.send('Just another API!');
+});
+
+app.listen(constants.PORT, err => {
     if (err){
         throw err;
     } else {
-        console.log(`
-        Server running on port : ${PORT}
-        ---------------------------------
-        Runnning on ${process.env.NODE_ENV}
-        ---------------------------------
-        MCM 2018/19 - Sistemas Distribuidos
-        `)
+      console.log(`
+      Server running on port : ${constants.PORT}
+      ---------------------------------
+      Runnning on ${process.env.NODE_ENV}
+      ---------------------------------
+      MCM 2018/19 - Sistemas Distribuidos
+      `)
     }
 })
